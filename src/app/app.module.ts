7f45b5478b9d4a3b9abb757428b918caca56387a
Cace57 <101878075+Cace57@users.noreply.github.com>
2022-03-22 20:49:22 +0100

    master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, PageComponent, SubPageComponent } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SearchBarComponent } from './components/header/search-bar/search-bar.component';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { NavLinksComponent } from './components/nav-bar/nav-links/nav-links.component';
-import { LangComponent } from './components/header/lang/lang.component';
-import {MatSelectModule} from '@angular/material/select';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
+import { LangComponent } from './components/footer/lang/lang.component';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
-import { MainPagesComponent } from './pages/main-pages/main-pages.component';
-import { AuthPageComponent } from './pages/auth-page/auth-page.component';
-import { HomePageComponent } from './pages/main-pages/home-page/home-page.component';
-import { FeedComponent } from './pages/main-pages/home-page/feed/feed.component';
+
 import { CarouselComponent } from './components/carousel/carousel.component';
-import { FooterComponent } from './footer/footer.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 
@@ -33,14 +30,11 @@ import { FooterComponent } from './footer/footer.component';
     SearchBarComponent,
     NavBarComponent,
     NavLinksComponent,
-    LangComponent,
-    MainPagesComponent,
-    AuthPageComponent,
-    HomePageComponent,
-    FeedComponent,
     CarouselComponent,
     FooterComponent,
-
+    PageComponent,
+    SubPageComponent,
+    LangComponent
 
 
 
@@ -54,7 +48,8 @@ import { FooterComponent } from './footer/footer.component';
     MatSelectModule,
     MatFormFieldModule,
     MatInputModule,
-  
+    AppRoutingModule
+
 
 
   ],
