102216697718ab1c725f941e78b3acb438507e7f
Naum <znaum12@gmail.com>
2022-03-26 14:09:31 +0100

    improved footer && added angular dark theme

--- src/app/components/footer/lang/lang.component.ts
+++ src/app/components/footer/lang/lang.component.ts
@@ -1,11 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Country {
+  value: string;
+  viewValue: string;
+}
 @Component({
   selector: 'app-lang',
   templateUrl: './lang.component.html',
   styleUrls: ['./lang.component.scss']
 })
 export class LangComponent implements OnInit {
+  countries: Country[] = [
+    {value: 'macedonian-0', viewValue: 'Македонски'},
+    {value: 'english-1', viewValue: 'English'},
+
+  ];
 
   constructor() { }
 
